78090a07d490318655f53229642e429f34d98d3b
Regis Blanc <regwblanc@gmail.com>
2016-09-15 17:29:14 +0200

    testing finite sorts

--- src/test/scala/cafesat/theories/adt/AdtSolverSignaturesTests.scala
+++ src/test/scala/cafesat/theories/adt/AdtSolverSignaturesTests.scala
@@ -17,6 +17,18 @@ class AdtSolverSignaturesTests extends FlatSpec {
     assert(Signature.isWellFounded(sigFinite3))
   }
 
+  it should "have all sorts finite" in {
+    val sigFinite1 = Seq(Seq(Seq()))
+    val sig1 = Signature(sigFinite1, Seq(Seq(Seq())))
+    assert(sig1.isFiniteSort(0))
+    val sigFinite2 = Seq(Seq(Seq(), Seq()))
+    val sig2 = Signature(sigFinite2, Seq(Seq(Seq(), Seq())))
+    assert(sig2.isFiniteSort(0))
+    val sigFinite3 = Seq(Seq(Seq(), Seq()), Seq(Seq()))
+    val sig3 = Signature(sigFinite3, Seq(Seq(Seq(), Seq()), Seq(Seq())))
+    assert(sig3.isFiniteSort(0))
+  }
+
   "Non recursive ADT" should "have a well-founded signature" in {
     val sigFinite1 = Seq(Seq(Seq()), Seq(Seq(0))) // one base sort, one simple wrapper constructor
     assert(Signature.isWellFounded(sigFinite1))
@@ -26,6 +38,29 @@ class AdtSolverSignaturesTests extends FlatSpec {
     assert(Signature.isWellFounded(sigFinite3))
     val sigFinite4 = Seq(Seq(Seq(), Seq(), Seq()), Seq(Seq(0), Seq(0)))
     assert(Signature.isWellFounded(sigFinite4))
+  }
+
+  it should "have all sorts finite" in {
+    val sigFinite1 = Seq(Seq(Seq()), Seq(Seq(0)))
+    val sig1 = Signature(sigFinite1, Seq(Seq(Seq()), Seq(Seq(Constructor(0,0,List())))))
+    assert(sig1.isFiniteSort(0))
+    assert(sig1.isFiniteSort(1))
+
+    val sigFinite2 = Seq(Seq(Seq(1)), Seq(Seq()))
+    val sig2 = Signature(sigFinite2, Seq(Seq(Seq(Constructor(0,0,List()))), Seq(Seq())))
+    assert(sig2.isFiniteSort(0))
+    assert(sig2.isFiniteSort(1))
+
+    val sigFinite3 = Seq(Seq(Seq(), Seq()), Seq(Seq(0)))
+    val sig3 = Signature(sigFinite3, Seq(Seq(Seq(), Seq()), Seq(Seq(Constructor(0,0,List())))))
+    assert(sig3.isFiniteSort(0))
+    assert(sig3.isFiniteSort(1))
+
+    val sigFinite4 = Seq(Seq(Seq(), Seq(), Seq()), Seq(Seq(0), Seq(0)))
+    val sig4 = Signature(sigFinite4, Seq(Seq(Seq(), Seq(), Seq()), 
+                                         Seq(Seq(Constructor(0,0,List())), Seq(Constructor(0,0,List())))))
+    assert(sig4.isFiniteSort(0))
+    assert(sig4.isFiniteSort(1))
   }
 
   "Recursive ADT" should "be well-founded if it has a base case" in {
